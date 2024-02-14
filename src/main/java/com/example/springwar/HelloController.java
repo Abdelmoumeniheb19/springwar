d2db18b1c7759c9ac203ecf29c0726821de63a1d
Abdelmoumeniheb19 <iabdelmoumen@algebrasystems.es>
2024-02-14 15:31:53 +0100

    3 commit

--- src/main/java/com/example/springwar/HelloController.java
+++ src/main/java/com/example/springwar/HelloController.java
@@ -10,7 +10,6 @@ import java.util.Map;
 @RestController
 public class HelloController {
 @GetMapping("/hello")
-@CrossOrigin("http://localhost:50588/")
     public Map<String, String> Hello(){
     Map<String, String> response = new HashMap<>();
     response.put("message", "Hello World!");
